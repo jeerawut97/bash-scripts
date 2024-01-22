f826ac8acf23424554a377a9e41c5e3b3e5338dd
JRW <jeerawut.jd40@gmail.com>
2024-01-22 21:36:05 +0700

    Tutorials - variables

---
+++ tutorials/variables.bash
@@ -0,0 +1,15 @@
+AGE=25
+echo $AGE
+
+# new values using a new assignment.
+AGE=35
+echo $AGE
+
+# make variables readonly can not assignment
+# readonly AGE
+
+# unset variable
+echo $AGE
+unset AGE
+echo "empty":$AGE
+
