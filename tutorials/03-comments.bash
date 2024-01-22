0d431367c037b09e8f7bc3d4a92381386ca20b54
JRW <jeerawut.jd40@gmail.com>
2024-01-22 21:40:43 +0700

    tutorial comment

---
+++ tutorials/03-comments.bash
@@ -0,0 +1,15 @@
+# Single-line comments.
+AGE=45
+echo "Age:$AGE"
+
+# Line1 comments
+# Line1 comments
+# Line1 comments
+
+# Multi-Line comments.
+: '
+multiline comments example1
+multiline comments example2
+multiline comments example3
+'
+echo "testing multi-line comments"
